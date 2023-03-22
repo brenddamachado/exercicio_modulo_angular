6d9dce29558dbc21bcbe78f05502f67143c9f502
brenddamachado <brenda16machado.bm@gmail.com>
2023-03-22 16:37:33 -0300

    atualizações

--- terceiro-projeto/src/app/shared/shared.module.ts
+++ terceiro-projeto/src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HeaderComponent } from './componentes/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule
   ]
