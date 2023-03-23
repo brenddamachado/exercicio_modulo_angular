1e806d0dbfaae0aec51b980153b17a35605998bc
brenddamachado <brenda16machado.bm@gmail.com>
2023-03-23 00:24:16 -0300

    atualização final

--- terceiro-projeto/src/app/app.component.ts
+++ terceiro-projeto/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Colaborador {
+  id: number;
+  nome: string;
+  salario: number; 
+  cargo: string;
+} 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'terceiro-projeto';
+  nathan: Colaborador = {
+    id: 7,
+    nome: "Nathan Carlos Exercicio",
+    salario: 4500,
+    cargo: "Dev Pl"
+  }
+  title: string = 'Listagem do colaborador';
+
+
 }
